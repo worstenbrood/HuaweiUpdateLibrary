80d1b040fc7f463f4b0247e0215b5efc3dada058
worstenbrood <worstenbrood@gmail.com>
2015-12-16 13:36:47 +0100

    Added Program.cs

---
+++ Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using HuaweiUpdateLibrary.Core;
+
+namespace Test
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            const string file = @"C:\Temp\update.app";
+            var updateFile = UpdateFile.Open(file);
+            updateFile[0].Extract(file, @"c:\temp\" + updateFile[0].FileType + ".img");
+        }
+    }
+}
