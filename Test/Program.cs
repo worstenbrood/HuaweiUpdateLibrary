9425df363f1c4fa857867beaa8dc8917f228606f
worstenbrood <worstenbrood@gmail.com>
2015-12-21 22:21:42 +0100

    Some tests

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,14 +1,46 @@
-﻿using HuaweiUpdateLibrary.Core;
+﻿using System;
+using System.IO;
+using System.Text;
+using HuaweiUpdateLibrary.Algorithms;
+using HuaweiUpdateLibrary.Core;
 
 namespace Test
 {
     static class Program
     {
+        public static void UpdateCrc16Test()
+        {
+            var crc = new UpdateCrc16();
+
+            var stest = "test";
+            var sum1 = BitConverter.ToUInt16(crc.ComputeHash(Encoding.ASCII.GetBytes(stest)), 0);
+
+            var btest1 = Encoding.ASCII.GetBytes("te");
+            var btest2 = Encoding.ASCII.GetBytes("st");
+            crc.TransformBlock(btest1, 0, 2, btest1, 0);
+            crc.TransformFinalBlock(btest2, 0, 2);
+
+            var sum2 = BitConverter.ToUInt16(crc.Hash, 0);
+
+            if (sum1 != 1928)
+                throw new Exception("Sum1 mismatch");
+
+            if (sum1 != sum2)
+                throw new Exception("Sum2 mismatch");
+        }
+
+
         static void Main(string[] args)
         {
             const string file = @"C:\Temp\update.app";
-            var updateFile = UpdateFile.Open(file);
-            updateFile.Extract(0, @"c:\temp\" + updateFile[0].FileType + ".img");
+            var u = UpdateFile.Create(@"c:\temp\test.app");
+            using (var stream = File.OpenRead(@"c:\temp\v1.14.zip"))
+            {
+                var e = UpdateEntry.Create();
+                u.Add(e, stream);
+            }
+            var v = UpdateFile.Open(@"c:\temp\test.app");
+            v.Extract(v[0], "c:\\temp\\test.zip");
         }
     }
 }
