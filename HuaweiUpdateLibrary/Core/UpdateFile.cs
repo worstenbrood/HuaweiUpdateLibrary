e58a41da9d97efa3d837533232531fa820571c8c
worstenbrood <worstenbrood@gmail.com>
2015-12-22 21:06:18 +0100

    Added newline

--- HuaweiUpdateLibrary/Core/UpdateFile.cs
+++ HuaweiUpdateLibrary/Core/UpdateFile.cs
@@ -10,6 +10,7 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *  You should have received a copy of the GNU General Public License along with HuaweiUpdateLibrary. 
  *  If not, see http://www.gnu.org/licenses/.
+ *  
  */
 
 using System.Collections;
